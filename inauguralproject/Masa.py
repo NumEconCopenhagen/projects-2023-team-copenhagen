94cf1c2dc96f6e99ea8bc8f35ae74ae787163c8a
zxb468 <zxb468@alumni.ku.dk>
2023-03-20 21:04:02 +0900

    Masa2

--- inauguralproject/Masa.py
+++ inauguralproject/Masa.py
@@ -112,8 +112,43 @@ class HouseholdSpecializationModelClass:
 
     def solve(self,do_print=False):
         """ solve model continously """
+        par = self.par
+        sol = self.sol
+        opt = SimpleNamespace()
+        N=1000
+        # a. all possible choices
+        x = np.linspace(0,24,N)
+        LM,HM,LF,HF = np.meshgrid(x,x,x,x) # all combinations
+    
+        LM = LM.ravel() # vector
+        HM = HM.ravel()
+        LF = LF.ravel()
+        HF = HF.ravel()
+
+        # b. calculate utility
+        u = self.calc_utility(LM,HM,LF,HF)
+    
+        # c. set to minus infinity if constraint is broken
+        I = (LM+HM > 24) | (LF+HF > 24) # | is "or"
+        u[I] = -np.inf
+    
+        # d. find maximizing argument
+        j = np.argmax(u)
         
-        pass    
+        opt.LM = LM[j]
+        opt.HM = HM[j]
+        opt.LF = LF[j]
+        opt.HF = HF[j]
+
+        # e. print
+        if do_print:
+            for k,v in opt.__dict__.items():
+                print(f'{k} = {v:6.4f}')
+
+        return opt
+
+        
+           
 
     def solve_wF_vec(self,discrete=False):
         """ solve model for vector of female wages """
